c55a9daf266e47ee9f3e1c1b37ede2ce58d397a5
Nihaal Korandla <nihaalrk@yahoo.com>
2018-09-03 16:18:39 -0400

    messing around with react-pose

--- react-client/src/components/pages/home_page.jsx
+++ react-client/src/components/pages/home_page.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { render } from "react-dom";
+import posed from "react-pose";
+
+const Box = posed.div({
+  hoverable: true,
+  pressable: true,
+  init: {
+    scale: 1,
+    boxShadow: '0px 0px 0px rgba(0,0,0,0)'
+  },
+  hover: {
+    scale: 1.2,
+    boxShadow: '0px 5px 10px rgba(0,0,0,0.2)'
+  },
+  press: {
+    scale: 1.1,
+    boxShadow: '0px 2px 5px rgba(0,0,0,0.1)'
+  }
+});
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        Home page
+      <div className="container">
+        <Box className="title-box">Nihaal Korandla's Personal Website</Box>
       </div>
-    )
+    );
   }
 }
 
